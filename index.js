0800cc5a4684c80345dc5b49b3eec4d1f7b38e5d
Mamutito <aithor123@gmail.com>
2024-12-16 15:11:55 +0000

    feat: add front react app

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 const { dbConnection } = require("./database/config");
 const port = process.env.PORT;
@@ -20,6 +21,10 @@ app.use(express.json());
 //Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/events", require("./routes/events"));
+
+app.use("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
